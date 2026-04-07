68451a4b48aecdebdf956f366b781fc0398b0a59
shunu859-byte <shunu859@gmail.com>
2026-04-08 01:51:48 +0600

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -67,16 +67,16 @@ module.exports = {
 
     // RANDOM GIFS
     const gifs = [
-      "https://i.imgur.com/Xw6JTfn.gif",
+      "https://i.imgur.com/zex8uo7.gif",
+      "https://i.imgur.com/4ki8eBI.gif",
+      "https://i.imgur.com/AMKQCJc.gif",
+      "https://i.imgur.com/rkjO7YV.gif",
+      "https://i.imgur.com/SgNPn8E.gif",
+      "https://i.imgur.com/u3qB5y2.gif",
       "https://i.imgur.com/KUFxWlF.gif",
       "https://i.imgur.com/FV9krHV.gif",
       "https://i.imgur.com/lFrFMEn.gif",
-      "https://i.imgur.com/KbcCZv2.gif",
-      "https://i.imgur.com/QC7AfxQ.gif",
-      "https://i.imgur.com/KrEez4A.gif",
-      "https://i.imgur.com/TtAOEAO.gif",
-      "https://i.imgur.com/mW0yjZb.gif",
-      "https://i.imgur.com/KQBcxOV.gif"
+      "https://i.imgur.com/KrEez4A.gif"
     ];
 
     const randomGif = gifs[Math.floor(Math.random() * gifs.length)];
